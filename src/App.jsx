3c4f07cc7c9d7ea78384e64c5b37bc1d1fd3f32b
Wahaj Khan <wahaj khan@shiwantek.local>
2026-02-18 20:07:23 +0500

    more changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import HeroSection from "./components/home/HeroSection";
 import Navbar from "./components/navbar/Navbar";
 import AIFeatures from "./components/home/AIFeatures";
 import AIEngines from "./components/home/AIEngines";
+import FavoriteApp from "./components/home/FavoriteApp";
  
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <HeroSection />   {/* ← pehle */}
       <AIFeatures/>
       <AIEngines/>
+      <FavoriteApp/>
     </main>
     </>
   );
