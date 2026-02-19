12243c1f4fd6ba4f26691b43fa3c3f4cb3b31111
hasanshiwantek <hasan.shiwantek@gmail.com>
2026-02-19 18:22:35 +0500

    Added happyuser page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import AIFeatures from "./components/home/AIFeatures";
 import AIEngines from "./components/home/AIEngines";
 import PricingPlan from "./components/home/PricingPlan";
 import FavoriteApp from "./components/home/FavoriteApp";
+import HappyUsers from "./components/home/HappyUsers";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <AIEngines />
         <FavoriteApp />
         <PricingPlan />
+        <HappyUsers/>
       </main>
     </>
   );
