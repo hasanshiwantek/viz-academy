93dbdc372a718de0304496a503576afd281bc482
Hanifa Anum <hanifaanum5@gmail.com>
2026-04-21 15:43:55 -0700

    Updated Webhook

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,1 @@
-export const ZAPIER_WEBHOOK = "https://hooks.zapier.com/hooks/catch/4692325/u4nzuft/u4nzuft.json";
+export const ZAPIER_WEBHOOK = "https://hooks.zapier.com/hooks/catch/4692325/u4nzuft";
